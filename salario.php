e83f133065d4ef1a0292119f6b5ebc38dbaee666
sarah1510 <sara.silvadefaria@hotmail.com>
2021-08-13 06:23:12 -0300

    first commit

---
+++ salario.php
@@ -0,0 +1,8 @@
+<php
+
+$nome-funcionario = $_POST["nome-funcionario"];
+$salario-atual = $_POST["salario-atual"];
+$genero = $_POST["genero"];
+$cargo = $_POST["cargo"];
+
+echo "teste para ver se sai algo na tela";
